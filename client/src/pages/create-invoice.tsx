e6a20b9f2631b6f4d4a48638f3cc083c637c835b
Emmanuel Fore~ <emmanuelfore22@gmail.com>
2026-01-19 11:44:09 +0200

    Fix another undefined selectedCompany in CreateInvoicePage PDF preview

--- client/src/pages/create-invoice.tsx
+++ client/src/pages/create-invoice.tsx
@@ -333,7 +333,7 @@ export default function CreateInvoicePage() {
                           currencySymbol: currentSymbol
                         }}
                         company={{
-                          ...selectedCompany,
+                          ...company,
                           bankName,
                           accountName,
                           accountNumber,

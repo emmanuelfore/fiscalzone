4b97ab78c3c47852f2ddc811d7d98134ec106146
Emmanuel Fore~ <emmanuelfore22@gmail.com>
2026-01-22 20:06:34 +0200

    Fix API handler to use built server code

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
-import { app } from '../server/index';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { app } = require('../dist/index.cjs');
 
 export default app;
